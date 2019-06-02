032dad6134810670e633ea230001f482adf59f9f
nataliesun <natsun121@gmail.com>
2019-06-02 14:49:55 -0700

    added jwt

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL:
     process.env.DB_URL ||
-    'postgresql://thingful:password@localhost:5432/thingful'
+    'postgresql://thingful:password@localhost:5432/thingful',
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
